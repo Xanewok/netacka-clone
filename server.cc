c6439c90e10569332390e83bf056fb4c53026743
Igor Matuszewski <Xanewok@gmail.com>
2017-06-10 13:25:13 +0200

    Make progress on server implementation

--- server.cc
+++ server.cc
@@ -4,8 +4,15 @@
 #include <string>
 #include <stdexcept>
 #include <limits>
+#include <ctime>
+#include <set>
+#include <memory>
 
+#include "protocol.h"
 #include "util.h"
+#include "rand.h"
+
+static Rand rand;
 
 static struct {
 	int width = 800;
@@ -14,7 +21,58 @@ static struct {
 	int rounds_per_sec = 50;
 	int turning_speed = 6;
 	int rand_seed;
+	bool seed_provided = false;
 } configuration;
+
+struct map {
+	std::vector<pixel*> pixels;
+};
+
+struct player_coords {
+	double x;
+	double y;
+	double rotation; // in degrees, clockwise, 0* = right
+};
+
+// Logically identified by (socket, session_id) pair
+struct player_connection {
+	constexpr static std::uint64_t CONNECTION_TIMEOUT = 2000; // [ms]
+
+	int socket;
+	std::uint64_t session_id;
+	std::string player_name;
+	bool ready_to_play = false;
+	bool is_waiting = false; // set to true if spectating, but wants to play next
+	std::uint64_t last_response_time;
+};
+
+struct name_compare
+{
+	bool operator() (const player_connection& lhs, const player_connection& rhs) const
+	{
+		return lhs.player_name < rhs.player_name;
+	}
+};
+
+bool in_map(double x, double y)
+{
+	std::uint64_t x_rounded = x;
+	std::uint64_t y_rounded = y;
+
+	return x_rounded >= 0 && x_rounded < configuration.width
+		&& y_rounded >= 0 && y_rounded < configuration.height;
+}
+
+static struct {
+	std::uint32_t game_id;
+	std::set<player_connection, name_compare> players;
+
+} game_state;
+
+void handle_client_message(client_message& msg)
+{
+
+}
 
 constexpr const char* usage_msg =
 "USAGE:  ./siktacka-server [-W n] [-H n] [-p n] [-s n] [-t n] [-r n]\n"
@@ -29,8 +87,8 @@ constexpr const char* usage_msg =
 
 namespace {
 	std::int64_t parse_param(const char* s,
-			std::int64_t min = std::numeric_limits<std::int64_t>::min(),
-			std::int64_t max = std::numeric_limits<std::int64_t>::max())
+		std::int64_t min = std::numeric_limits<std::int64_t>::min(),
+		std::int64_t max = std::numeric_limits<std::int64_t>::max())
 	{
 		std::int64_t param;
 		try
@@ -50,6 +108,7 @@ namespace {
 
 int main(int argc, char* argv[])
 {
+	// Parse optional arguments
 	for (int i = 1; i < argc; i += 2)
 	{
 		const char* arg = argv[i];
@@ -60,27 +119,29 @@ int main(int argc, char* argv[])
 		{
 			printf("Bad argument: %s%s\n%s",
 				arg, (i + 1 >= argc ? " (missing parameter)" : ""), usage_msg);
-			std::exit(1);			
+			std::exit(1);
 		}
 
 		std::int64_t param = 0;
 		if (arg[1] != 'p')
-		std::uint64_t param = parse_param(argv[i + 1]);
+			std::uint64_t param = parse_param(argv[i + 1]);
 		printf("Parsed param: %lld\n", param);
 		switch (arg[1])
 		{
-			case 'W': configuration.width          = param; break;
-			case 'H': configuration.height         = param; break;
-			case 'p': configuration.port_num       = util::parse_bounded(argv[i + 1], 0, 65535); break;
-			case 's': configuration.rounds_per_sec = param; break;
-			case 't': configuration.turning_speed  = param; break;
-			case 'r': configuration.rand_seed      = param; break;
-			default:
-			{
-				printf("Bad argument: %s\n%s", arg, usage_msg);
-				std::exit(1);			
-			}
+		case 'W': configuration.width = param; break;
+		case 'H': configuration.height = param; break;
+		case 'p': configuration.port_num = util::parse_bounded(argv[i + 1], 0, 65535); break;
+		case 's': configuration.rounds_per_sec = param; break;
+		case 't': configuration.turning_speed = param; break;
+		case 'r': configuration.rand_seed = param; configuration.seed_provided = true; break;
+		default:
+		{
+			printf("Bad argument: %s\n%s", arg, usage_msg);
+			std::exit(1);
+		}
 		}
 	}
+	// Initialize deterministic random generator
+	rand = Rand(configuration.seed_provided ? configuration.rand_seed : time(nullptr));
 }
 
